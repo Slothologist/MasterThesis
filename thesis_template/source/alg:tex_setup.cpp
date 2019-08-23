e1e057caf2a929d6b11d2868f5371034275a0c09
rfeldhans <rfeldhans@techfak.uni-bielefeld.de>
2019-08-23 18:09:19 +0200

    first feedback on motivation; improved related work; started on eval, just note till nw though

---
+++ thesis_template/source/alg:tex_setup.cpp
@@ -0,0 +1,4 @@
+GenTextures(1, &id); //Generieren einer Textur, deren \gls{glid} in id gespeichert wird
+//Festsetzen von Texturparametern (Funktionsparameter zwecks Übersichtlichkeit ausgelassen)
+TexParametri();
+TexImage2D();
